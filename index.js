4725bea7af0a1d0d684561fbaaa6df6fd64954fc
Alan Ayala <aaga941228@outlook.es>
2020-06-06 21:46:44 -0500

    tags

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello");
+const grid = new Muuri(".grid", {
+  layout: {
+    rounding: false,
+  },
+});
+
+window.addEventListener("load", () => {
+  grid.refreshItems().layout();
+  document.querySelector("#grid").classList.add("imagenes-cargadas");
+
+  const enlaces = document.querySelectorAll("#categorias a");
+  enlaces.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      event.preventDefault();
+      enlaces.forEach((enlace) => enlace.classList.remove("activo"));
+      event.target.classList.add("activo");
+
+      const categoria = event.target.innerHTML.toLowerCase();
+      categoria === "todos"
+        ? grid.filter("[data-categoria]")
+        : grid.filter(`[data-categoria="${categoria}"]`);
+    });
+  });
+});
