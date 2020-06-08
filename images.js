7a7cd3ee62da0ddd123112f3b38bc16cf22d5b7c
Alan Ayala <aaga941228@outlook.es>
2020-06-07 23:37:55 -0500

    gitignore

--- images.js
+++ images.js
@@ -1,90 +1,90 @@
 const images = [
     {
-        path: '/images/kotegawa1.jpg',
+        path: 'images/kotegawa1.jpg',
         etiquetas: 'anime kotegawa',
         categoria: 'anime',
         descripcion: 'Yui Kotegawa'
     },
     {
-        path: '/images/kotegawa2.jpg',
+        path: 'images/kotegawa2.jpg',
         etiquetas: 'anime kotegawa',
         categoria: 'anime',
         descripcion: 'Yui Kotegawa'
     },
     {
-        path: '/images/kotegawa3.jpg',
+        path: 'images/kotegawa3.jpg',
         etiquetas: 'anime kotegawa',
         categoria: 'anime',
         descripcion: 'Yui Kotegawa'
     },
     {
-        path: '/images/sawachika1.jpg',
+        path: 'images/sawachika1.jpg',
         etiquetas: 'anime sawachika',
         categoria: 'anime',
         descripcion: 'Eri Sawachika'
     },
     {
-        path: '/images/sawachika2.jpg',
+        path: 'images/sawachika2.jpg',
         etiquetas: 'anime sawachika',
         categoria: 'anime',
         descripcion: 'Eri Sawachika'
     },
     {
-        path: '/images/sawachika3.jpg',
+        path: 'images/sawachika3.jpg',
         etiquetas: 'anime sawachika',
         categoria: 'anime',
         descripcion: 'Eri Sawachika'
     },
     {
-        path: '/images/zelda1.jpg',
+        path: 'images/zelda1.jpg',
         etiquetas: 'videojuegos zelda',
         categoria: 'videojuegos',
         descripcion: 'Zelda'
     },
     {
-        path: '/images/zelda2.jpg',
+        path: 'images/zelda2.jpg',
         etiquetas: 'videojuegos zelda',
         categoria: 'videojuegos',
         descripcion: 'Zelda'
     },
     {
-        path: '/images/zelda3.jpg',
+        path: 'images/zelda3.jpg',
         etiquetas: 'videojuegos zelda',
         categoria: 'videojuegos',
         descripcion: 'Zelda'
     },
     {
-        path: '/images/palutena1.jpg',
+        path: 'images/palutena1.jpg',
         etiquetas: 'videojuegos palutena',
         categoria: 'videojuegos',
         descripcion: 'Palutena'
     },
     {
-        path: '/images/palutena2.jpg',
+        path: 'images/palutena2.jpg',
         etiquetas: 'videojuegos palutena',
         categoria: 'videojuegos',
         descripcion: 'Palutena'
     },
     {
-        path: '/images/palutena3.jpg',
+        path: 'images/palutena3.jpg',
         etiquetas: 'videojuegos palutena',
         categoria: 'videojuegos',
         descripcion: 'Palutena'
     },
     {
-        path: '/images/bayonetta1.jpg',
+        path: 'images/bayonetta1.jpg',
         etiquetas: 'videojuegos bayonetta',
         categoria: 'videojuegos',
         descripcion: 'Bayonetta'
     },
     {
-        path: '/images/bayonetta2.jpg',
+        path: 'images/bayonetta2.jpg',
         etiquetas: 'videojuegos bayonetta',
         categoria: 'videojuegos',
         descripcion: 'Bayonetta'
     },
     {
-        path: '/images/bayonetta3.jpg',
+        path: 'images/bayonetta3.jpg',
         etiquetas: 'videojuegos bayonetta',
         categoria: 'videojuegos',
         descripcion: 'Bayonetta'
